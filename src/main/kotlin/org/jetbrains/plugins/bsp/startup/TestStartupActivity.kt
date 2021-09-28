6b047a3d45dffe41ef6d76e6e82fb2b2a7c89bcd
Marcin Abramowicz <Marcin.Abramowicz@jetbrains.com>
2021-09-28 17:24:29 +0200

    workspace model preparation

---
+++ src/main/kotlin/org/jetbrains/plugins/bsp/startup/TestStartupActivity.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.plugins.bsp.startup
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+
+class TestStartupActivity : StartupActivity {
+
+  override fun runActivity(project: Project) {
+    TODO("Not yet implemented")
+  }
+}
